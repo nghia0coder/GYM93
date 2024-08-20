940b5672438e62aa061e72322569de6fa9eb67ae
nghia0coder <nghialmao@gmail.com>
2024-08-20 19:47:41 +0700

    Add project files.

---
+++ GYM93/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GYM93.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
