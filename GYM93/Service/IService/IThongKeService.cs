c87a613112df4118363d053ce37cce0b2315303e
nghia0coder <nghialmao@gmail.com>
2024-09-03 23:34:12 +0700

    feat  : implement Revenue report in Home Index

---
+++ GYM93/Service/IService/IThongKeService.cs
@@ -0,0 +1,12 @@
+﻿using GYM93.Models.ViewModels;
+
+namespace GYM93.Service.IService
+{
+    public interface IThongKeService
+    {
+        Task<List<MonthlyRevenue>> GetMonthlyRevenues();
+
+        Task<List<MonthlyRegistrations>> GetMonthlyRegistrations();
+        Task<List<MontlyCompareRevenue>> GetMonthlyRevenueGrowth();
+    }
+}
